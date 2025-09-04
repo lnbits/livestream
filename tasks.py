d22f715226b8065385cf185b7ce754b0641e4327
Tiago Vasconcelos <talvasconcelos@gmail.com>
2025-09-04 17:03:17 +0100

    fix tasks

--- tasks.py
+++ tasks.py
@@ -1,9 +1,10 @@
 import asyncio
 
+from loguru import logger
+
 from lnbits.core.models import Payment
 from lnbits.core.services import create_invoice, pay_invoice
 from lnbits.tasks import register_invoice_listener
-from loguru import logger
 
 from .crud import get_livestream_by_track, get_producer, get_track
 
@@ -18,7 +19,6 @@ async def wait_for_paid_invoices():
 
 
 async def on_invoice_paid(payment: Payment) -> None:
-
     if not payment.extra or payment.extra.get("tag") != "livestream":
         # not a livestream invoice
         return
@@ -40,18 +40,18 @@ async def on_invoice_paid(payment: Payment) -> None:
 
     amount = int(payment.amount * (100 - ls.fee_pct) / 100)
 
-    payment = await create_invoice(
+    producer_invoice = await create_invoice(
         wallet_id=producer.wallet,
         amount=int(amount / 1000),
         internal=True,
         memo=f"Revenue from '{track.name}'.",
     )
     logger.debug(
-        f"livestream: producer invoice created: {payment.payment_hash}, {amount} msats"
+        f"livestream: producer invoice created: {producer_invoice.payment_hash}, {amount} msats"
     )
 
-    payment = await pay_invoice(
-        payment_request=payment.bolt11,
+    payment_to_producer = await pay_invoice(
+        payment_request=producer_invoice.bolt11,
         wallet_id=payment.wallet_id,
         extra={
             **payment.extra,
@@ -59,7 +59,9 @@ async def on_invoice_paid(payment: Payment) -> None:
             "received": payment.amount,
         },
     )
-    logger.debug(f"livestream: producer invoice paid: {payment.checking_id}")
+    logger.debug(
+        f"livestream: producer invoice paid: {payment_to_producer.checking_id}"
+    )
 
     # so the flow is the following:
     # - we receive, say, 1000 satoshis
